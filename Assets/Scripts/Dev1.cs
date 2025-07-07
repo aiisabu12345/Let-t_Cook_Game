b92c8c48126c61ab6f59cecf3fa2f5e4656a0336
DESKTOP-IUUH71E\siwakon <siwkogod@gmail.com>
2025-07-07 10:34:11 +0700

    Test conflict

--- Assets/Scripts/Dev1.cs
+++ Assets/Scripts/Dev1.cs
@@ -5,7 +5,7 @@ public class Dev1 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("hello dev2");
     }
 
     // Update is called once per frame
